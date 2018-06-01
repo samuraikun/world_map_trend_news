8a9ba39f452efe7996dd1bb636eb4d7a7f4d056b
samuraikun <tazitaziawawa@gmail.com>
2018-06-02 00:47:06 +0900

    add news api

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express  = require('express');
 require('dotenv').config();
+const NewsAPI = require('newsapi');
+const news_api = new NewsAPI(process.env.NEWS_API_KEY);
 
 const app = express();
+
+app.get('/api/news/recent', async (req, res) => {
+  try {
+    const response = await news_api.v2.topHeadlines({
+      category: req.query.category,
+      country: req.query.country
+    });
+
+    res.send(response);
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('client/build'));
