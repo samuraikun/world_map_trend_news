b7d794a24f2852486cb37dd37b22da85ebb70574
samuraikun <tazitaziawawa@gmail.com>
2018-06-02 14:18:37 +0900

    alternate map lib from react-map-gl to react-simple-maps

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,83 @@
 import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
+import axios from 'axios';
+import worldmap from './world-50m.json';
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography
+} from 'react-simple-maps';
 
-// set mapbox access token
-const MAPBOX_TOKEN = '';
+const wrapperStyles = {
+  width: "80%",
+  height: "80%",
+  margin: "0 auto"
+}
+
+const geoStyles = {
+  default: {
+    fill: "#00CC33",
+    stroke: "#607D8B",
+    strokeWidth: 0.75,
+    outline: "none",
+  },
+  hover: {
+    fill: "#607D8B",
+    stroke: "#607D8B",
+    strokeWidth: 0.75,
+    outline: "none",
+  },
+  pressed: {
+    fill: "#FF5722",
+    stroke: "#607D8B",
+    strokeWidth: 0.75,
+    outline: "none",
+  }
+}
 
 class Map extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewport: {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        latitude: 35.681167,
-        longitude: 139.767052,
-        zoom: 3
-      }
-    };
+  async handleClick() {
+    try {
+      const response = await axios.get('/api/news/sources?country=us&language=en');
+
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
-    const {viewport} = this.state;
     return (
-      <ReactMapGL
-        {...viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-      />
-    );
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projectionConfig={{
+            scale: 205,
+            rotation: [-11,0,0],
+          }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+          >
+          <ZoomableGroup center={[20,30]} disablePanning>
+            <Geographies geography={worldmap}>
+              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  onClick={this.handleClick}
+                  style={geoStyles}
+                />
+              ))}
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
   }
 }
 
 export default Map;
-console.log(process.env.MapboxAccessToken);
